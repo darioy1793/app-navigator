f104780ac792521f46bc8826dce1bf4ebd90d84a
darioy1793 <darioy1793@gmail.com>
2026-02-13 08:10:32 -0500

    pantallaPersona

--- src/navigator/StackNavigator.tsx
+++ src/navigator/StackNavigator.tsx
@@ -2,15 +2,23 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { Pantalla1Screen } from '../screens/Pantalla1Screen';
 import { Pantalla2Screen } from '../screens/Pantalla2Screen';
 import { Pantalla3Screen } from '../screens/Pantalla3Screen';
+import { PersonaScreen } from '../screens/PersonaScreen';
 
 const Stack = createStackNavigator();
 
 export const StackNavigator = () => {
+
+  
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Pantalla1" component={Pantalla1Screen} />
-      <Stack.Screen name="Pantalla2" component={Pantalla2Screen} />
-      <Stack.Screen name="Pantalla3" component={Pantalla3Screen} />
+    //screenOptions: para configurar las opciones de todas las pantallas del stack navigator
+    <Stack.Navigator  screenOptions ={{cardStyle:{backgroundColor:'green'}, /* headershown:false */
+      headerStyle:{elevation:4}
+    }}>
+
+      <Stack.Screen name="Pantalla1" options={{title:'HOME'}} component={Pantalla1Screen} />
+      <Stack.Screen name="Pantalla2" options={{title:'Lista Producto'}} component={Pantalla2Screen} />
+      <Stack.Screen name="Pantalla3" options={{title:'Configuraciones'}} component={Pantalla3Screen} />
+      <Stack.Screen name="Persona" options={{title:'Lista Persona'}} component={PersonaScreen} />
     </Stack.Navigator>
   );
 }
--- src/screens/Pantalla1Screen.tsx
+++ src/screens/Pantalla1Screen.tsx
@@ -4,13 +4,29 @@ import { Button, Text, View } from 'react-native'
 import { stylesGlobal } from '../theme/appTheme';
 
 interface Props extends StackScreenProps<any, any>{};
+
+interface Persona{
+    id: number;
+    nombre: string;   
+    edad: number;
+}
+
 export const Pantalla1Screen = ({navigation}:Props) => {
+
+  const persona: Persona = {
+    id: 1,
+    nombre: 'Viviana',
+    edad: 34
+  }
+
     //console.log(props);
   return (
     <View style={stylesGlobal.container}>
         <Text style={stylesGlobal.title} > Pantalla 1 Screen </Text>
         <Button title= 'Ir pantalla 2'
         onPress={()=> navigation.navigate('Pantalla2')} />
+          <Button title= 'Ir persona'
+        onPress={()=> navigation.navigate('Persona',persona)} />
     </View>
   )
 }
